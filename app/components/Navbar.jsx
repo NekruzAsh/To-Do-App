1aaa7dc95fc4d322202364788df420645ffdb1cd
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-16 22:11:17 -0500

    Update Navbar styling

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,9 +5,14 @@ const Navbar = () => {
     //wrapper div
     <div className=''>
 
-        <div className='font-bold uppecase py-6 px-16 '>
-            <ul className='text-purple-600'>
-                <li>Make a note</li>
+        <div className='bg-[#bbb7b7] font-bold uppecase py-6 px-16 pb-6 border-b-[3px] border-[#222222]'>
+            <ul className='text-purple-600 text-center text-2xl'>
+              <button className='group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow'>
+                <div className='absolute inset-0 w-3 bg-purple-400 transition-all duration-[250ms] ease-out group-hover:w-full'></div>
+                  <li className='relative text-black group-hover:text-white'>
+                  Make a note
+                </li> 
+                </button>
             </ul>
         </div>
 
