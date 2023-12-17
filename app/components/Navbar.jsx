71fe98268508241b5becbd41b0e63f47701d7248
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-16 20:28:37 -0500

    Add Navbar component and update global styles

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    //wrapper div
+    <div className=''>
+
+        <div>
+            <ul className='text-purple-700'>
+                <li>Make a note</li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,11 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+//components
+import Navbar from './components/Navbar.jsx'
+
+
 export const metadata = {
   title: 'My To-Do App',
   description: 'A simple to-do app built with Next.js and Tailwind CSS.',
@@ -12,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        
+        <Navbar />
           
         
         
