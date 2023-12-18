6ba2c12b8feb15b395bd9960cd1efbaf5f42becf
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-18 15:18:12 -0500

    Fix bug in login functionality
    Update README with new instructions
    
    Refactor code for better performance
    
    Add new feature for user authentication
    
    Fix formatting issues in code
    
    Update dependencies to latest versions
    
    Remove unused variables
    
    Fix broken links in documentation
    
    Add error handling for edge cases

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -2,9 +2,27 @@
 import React from "react";
 import { useState } from "react";
 
-const Navbar = () => {
+const TodoAPP = () => {
+
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+
+  const addNote = () => {
+    if (newNote.trim() !== '') {
+      setNotes([...notes, newNote]);
+      setNewNote('');
+    }
+  };
+
+  const deleteNote = (index) => {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+  };
+
+
   return (
-    
+
     <div className="min-h-screen flex flex-col items-center justify-center">
   <div>
     <h1 className="uppercase py-4 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-400 to-indigo-500">To-do App</h1>
@@ -15,16 +33,32 @@ const Navbar = () => {
         className="shadow-sm shadow-gray-500 rounded-sm border-2 border-gray-300 w-68 h-40 p-4"
         placeholder="Type your note..."
         name="text"
+        value={newNote}
+        onChange={(e) => setNewNote (e.target.value)}
       ></textarea>
 
       <ul className="text-purple-600 text-center text-2xl mt-6 lg:mt-12">
-        <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
+        <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow"
+        onClick={addNote}>
           <div className="absolute inset-0 w-3 bg-purple-400 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
           <li className="relative text-black group-hover:text-white">
             Make a note
           </li>
         </button>
       </ul>
+      <ul className="text-purple-600 text-2xl mt-6">
+          {notes.map((note, index) => (
+            <li key={index} className="flex items-center justify-between mb-2">
+              {note}
+              <button
+                className="text-red-500"
+                onClick={() => deleteNote(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
     </div>
   </div>
 </div>
@@ -32,4 +66,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default TodoAPP;
