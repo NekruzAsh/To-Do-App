e6f4e2f5f8e896e43c726fd114de8011a7358c24
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-17 20:31:11 -0500

    Refactor Navbar component to include a dynamic resizing text box

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
 
 const Navbar = () => {
   return (
-    //wrapper div
-    <div className=''>
+    <div className="container mx-auto min-h-screen flex items-center justify-center">
+      <div className="w-64 h-64">
+      {/* Dynamic Resizing Text Box */}
+      <textarea
+        type="text"
+        className="w-full h-full px-6 py-10 border rounded-lg border-purple-400 text-md resize-y flex items-center justify-center"
+        style={{ height: "auto" }}
+        placeholder="Type your note..."
+      />
 
-        <div className='bg-[#bbb7b7] font-bold uppecase py-6 px-16 pb-6 border-b-[3px] border-[#222222]'>
-            <ul className='text-purple-600 text-center text-2xl'>
-              <button className='group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow'>
-                <div className='absolute inset-0 w-3 bg-purple-400 transition-all duration-[250ms] ease-out group-hover:w-full'></div>
-                  <li className='relative text-black group-hover:text-white'>
-                  Make a note
-                </li> 
-                </button>
-            </ul>
-        </div>
-
+      <ul className="text-purple-600 text-center text-2xl">
+        <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
+          <div className="absolute inset-0 w-3 bg-purple-400 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+          <li className="relative text-black group-hover:text-white">
+            Make a note
+          </li>
+        </button>
+      </ul>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
