cb508feadec17d088a2ba603d278c40b033e571d
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-19 16:04:06 -0500

    Add table with information and update dependencies for json server

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -67,6 +67,25 @@ const TodoAPP = () => {
     </div>
   ))}
 </div>
+<div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        
+        <th>Name</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr className="hover:text-white hover:bg-indigo-600">
+        <td>Cy Ganderton</td>
+        <td>Blue</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </div>
 
   );
