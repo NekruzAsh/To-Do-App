49c99b70daf48e6c535560c556456451a8621949
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-18 20:51:15 -0500

    added note display in TodoApp component

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -23,18 +23,18 @@ const TodoAPP = () => {
 
   return (
 
-    <div className="min-h-screen flex flex-col items-center justify-center">
+<div className="min-h-screen flex flex-col items-center justify-center">
   <div>
     <h1 className="uppercase py-4 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-400 to-indigo-500">To-do App</h1>
   </div>
   <div className="bg-purple-300 p-6 rounded-md shadow-md shadow-gray-500 mt-[70px]">
     <div className="grid lg:grid-cols-2 lg:gap-6">
       <textarea
-        className="shadow-sm shadow-gray-500 rounded-sm border-2 border-gray-300 w-68 h-40 p-4"
+        className="shadow-sm shadow-gray-500 rounded-sm border-2 border-gray-300 w-full min-h-[100px] p-4 resize-none"
         placeholder="Type your note..."
         name="text"
         value={newNote}
-        onChange={(e) => setNewNote (e.target.value)}
+        onChange={(e) => setNewNote(e.target.value)}
       ></textarea>
 
       <ul className="text-purple-600 text-center text-2xl mt-6 lg:mt-12">
@@ -46,20 +46,25 @@ const TodoAPP = () => {
           </li>
         </button>
       </ul>
-      <ul className="text-purple-600 text-2xl mt-6">
-          {notes.map((note, index) => (
-            <li key={index} className="flex items-center justify-between mb-2">
-              {note}
-              <button
-                className="text-red-500"
-                onClick={() => deleteNote(index)}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
     </div>
+  </div>
+  <div className="grid grid-cols-2 gap-4 mt-6">
+    {notes.map((note, index) => (
+      <div
+        key={index}
+        className="bg-yellow-200 p-4 rounded-md shadow-md flex items-center justify-between"
+      >
+        <span>
+          {note}
+        </span>
+        <button
+          className="text-red-500"
+          onClick={() => deleteNote(index)}
+        >
+          Delete
+        </button>
+      </div>
+    ))}
   </div>
 </div>
 
